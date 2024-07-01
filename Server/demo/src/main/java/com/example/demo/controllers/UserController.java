e8a4d33caf1d9640202fb3c735a4ac682f0227ff
NikolajHon <goncharenkokolya3@gmail.com>
2024-07-01 18:47:16 +0200

    Add server

---
+++ Server/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
